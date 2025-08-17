0dee3d74c6381e0b8fca1854254e1b7da5b6230d
Alexandr Pumnea <apumnea.dev@gmail.com>
2025-08-18 00:15:59 +0300

    add: FileReader implementation that reads data from txt file and creates a profile object.

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,30 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
+            Profile profile = new Profile();
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("Name: ")) {
+                    profile.setName(line.replace("Name: ", ""));
+                } else if (line.startsWith("Age: ")) {
+                    profile.setAge(Integer.parseInt(line.replace("Age: ", "")));
+                } else if (line.startsWith("Email: ")) {
+                    profile.setEmail(line.replace("Email: ", ""));
+                } else if (line.startsWith("Phone: ")) {
+                    profile.setPhone(Long.parseLong(line.replace("Phone: ", "")));
+                }
+            }
+            return profile;
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file: " + file.getName(), e);
+        }
     }
 }
